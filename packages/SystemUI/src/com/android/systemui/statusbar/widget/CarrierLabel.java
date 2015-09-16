c35e21f9b1185f7221c25d158099c7495402d7ab
winga45 <Winga45@gmail.com>
2015-09-16 15:48:04 +0000

    Revert "rework carrier label 2/2"
    This reverts commit e8b7b01bf504816ae75d480421f9d0f3df38e98a.

--- packages/SystemUI/src/com/android/systemui/statusbar/widget/CarrierLabel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/widget/CarrierLabel.java
@@ -49,7 +49,7 @@ public class CarrierLabel extends TextView {
     private static boolean isCN;
     private int mCarrierFontSize = 14;
 
-    protected int mCarrierColor = getResources().getColor(R.color.kg_carrier_text);
+    protected int mCarrierColor = com.android.internal.R.color.white;
     Handler mHandler;
 
     class SettingsObserver extends ContentObserver {
