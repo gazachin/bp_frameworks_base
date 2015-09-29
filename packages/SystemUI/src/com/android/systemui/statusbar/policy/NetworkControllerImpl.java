fe96d53cd07e61e17b5dd4e0e21cb05ab5984fdd
winga45 <Winga45@gmail.com>
2015-09-29 16:35:38 +0000

    FINAL FIX pt3 to Normal and Fully connect colors Thanks to xxmrhyde for his help
    Change-Id: I8a27305ded69a7d8872be4b7979ad492dec13a68

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -918,7 +918,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
             int signalClustersLength = mSignalClusters.size();
             for (int i = 0; i < signalClustersLength; i++) {
                 mSignalClusters.get(i).setWifiIndicators(wifiVisible, getCurrentIconId(),
-                        getActivityIconId(ssidPresent), contentDescription);
+                mCurrentState.inetCondition, mWifiActivityIconId, contentDescription);
             }
         }
 
@@ -1282,7 +1282,8 @@ public class NetworkControllerImpl extends BroadcastReceiver
                 mSignalClusters.get(i).setMobileDataIndicators(
                         mCurrentState.enabled && !mCurrentState.airplaneMode,
                         getCurrentIconId(),
-                        getActivityIconId(mCurrentState.dataConnected),
+                        mCurrentState.inetCondition,
+                        mMobileActivityIconId,
                         typeIcon,
                         contentDescription,
                         dataContentDescription,
@@ -1890,10 +1891,10 @@ public class NetworkControllerImpl extends BroadcastReceiver
     }
 
     public interface SignalCluster {
-        void setWifiIndicators(boolean visible, int strengthIcon,
-                int activityIcon, String contentDescription);
+        void setWifiIndicators(boolean visible, int strengthIcon, int inetCondition, int activityIcon,
+                String contentDescription);
 
-        void setMobileDataIndicators(boolean visible, int strengthIcon, int activityIcon,
+        void setMobileDataIndicators(boolean visible, int strengthIcon, int inetCondition, int activityIcon,
                 int typeIcon, String contentDescription, String typeContentDescription,
                 boolean isTypeIconWide, boolean showRoamingIndicator, int subId);
         void setSubs(List<SubscriptionInfo> subs);
