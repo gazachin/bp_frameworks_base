e3f47f9b8d24c36ffc26c61f1ce498a1254290f5
winga45 <Winga45@gmail.com>
2015-10-01 18:42:00 +0000

    Revert "fast charge Tile 2/2"
    This reverts commit 5fe4218b5ba1eaa7b14b862a8e3fc77db655986c.

--- core/java/com/android/internal/util/bliss/DeviceUtils.java
+++ core/java/com/android/internal/util/bliss/DeviceUtils.java
@@ -39,7 +39,6 @@ import android.view.WindowManager;
 
 import com.android.internal.telephony.PhoneConstants;
 
-import java.io.File;
 public class DeviceUtils {
 
     // Device types
@@ -117,14 +116,6 @@ public class DeviceUtils {
 
     public static boolean isTablet(Context con) {
         return getScreenType(con) == DEVICE_TABLET;
-    }
-
-    public static boolean fchargeEnabled(Context con) {
-        String fchargePath = con.getString(com.android.internal.R.string.config_fastChargePath);
-        if (fchargePath == null || fchargePath.isEmpty() || !new File(fchargePath).exists()) {
-            return false;
-        }
-        return true;
     }
 
     private static int getScreenType(Context con) {
